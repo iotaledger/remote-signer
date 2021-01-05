258ff7d581f959f6cd4ddbae6423aa1ebb0a587e
galrogo <gal.rogozinski@iota.org>
2021-01-05 10:29:19 +0200

    change public key for testing

--- src/dispatcher/client.rs
+++ src/dispatcher/client.rs
@@ -9,12 +9,11 @@ pub mod dispatcher {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = SignatureDispatcherClient::connect("http://dispatcher.remote-signer:50051").await?;
+    let mut client = SignatureDispatcherClient::connect("http://localhost:50051").await?;
 
     let request = tonic::Request::new(SignMilestoneRequest {
         pub_keys: vec![
-            Vec::from_hex("d578d0a8e5392040cf5c3ba0153c28c300d8eaed3d8d7ef43729cadfe1e2467b").unwrap(),
-            Vec::from_hex("268783e7277c7c9f6dc898d08f5ca458941de2eb339e7da948239cc3647dffcc").unwrap()
+            Vec::from_hex("3a0f0d32ed6e427c581da7ac52d22e727bee48c4af74a1850a57a2047c1e387e").unwrap()
         ],
         ms_essence: "Sign this!".as_bytes().to_vec()
     });
